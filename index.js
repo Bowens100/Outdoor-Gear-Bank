cf7afe16770001b5704d6cf9e3c01428f920ae39
Bowens100 <77711399+Bowens100@users.noreply.github.com>
2022-06-28 20:53:02 -0400

    Added create functionality for products

--- index.js
+++ index.js
@@ -21,14 +21,20 @@ app.get('/products', async (req, res) => {
     res.render('index', {products});
 })
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
 app.get('/products/:id', async (req, res) => {
     const {id} = req.params;
     const product = await Product.findById(id);
-    res.render('show', {product});
+    res.render('products/show', {product});
 })
 
-app.post('/products', (req, res) => {
-
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {
